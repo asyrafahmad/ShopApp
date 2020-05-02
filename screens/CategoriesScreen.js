e6f7720021e23ed25d93f6b6160caec144dc95c0
asyrafahmad <are.syraf9764@gmail.com>
2020-05-02 16:48:16 +0800

    117. Outputting a Grid of Categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { View, Text,Button, StyleSheet } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = itemData  => {
+    return (
+        <View style={styles.gridItem}>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    )
+};
 
 const CategoriesScreen = props => {
 
-    console.log(props);
+     //console.log(props);
 
     return(
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-            <Button title="Go to  Meals"  onPress={() => {
-                props.navigation.navigate({routeName: 'CategoryMeals'})  // or similar to (props.navigation.navigate('CategoryMeals');)
-            }} />
-        </View>
+        <FlatList 
+            keyExtractor={(item, index) => item.id} 
+            data={CATEGORIES} 
+            renderItem={renderGridItem} 
+            numColumns={2} 
+        />
+
+        // <View style={styles.screen}>
+        //     <Text>The Categories Screen!</Text>
+        //     <Button title="Go to  Meals"  onPress={() => {
+        //         props.navigation.navigate({routeName: 'CategoryMeals'})  // or similar to (props.navigation.navigate('CategoryMeals');)
+        //     }} />
+        // </View>
     );
 };
 
@@ -20,6 +37,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
