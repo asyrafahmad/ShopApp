2720d5447ec8d20542739401e10c1d83cefbe908
asyrafahmad <are.syraf9764@gmail.com>
2020-05-22 00:38:49 +0800

    129. Adding Tabs-based Navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,32 +1,42 @@
 import { createAppContainer} from 'react-navigation';
 import { createStackNavigator} from 'react-navigation-stack';
+import { createBottomTabNavigator} from 'react-navigation-tabs';
 import { Platform } from 'react-native';
 
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
-const MealsNavigator = createStackNavigator({
-    Categories: {
-        screen: CategoriesScreen
-    },
-    CategoryMeals: {
-        screen: CategoryMealsScreen,
-    },
-    MealDetail: MealDetailScreen
-}, 
-{ 
-    // mode: 'modal',                  // for IOS animation similar to Android (modal transition)
+const MealsNavigator = createStackNavigator(
+    {
+        Categories: {
+            screen: CategoriesScreen
+        },
+        CategoryMeals: {
+            screen: CategoryMealsScreen,
+        },
+        MealDetail: MealDetailScreen
+    }, 
+    { 
+        // mode: 'modal',                  // for IOS animation similar to Android (modal transition)
 
-    initialRouteName: 'Categories',
-    defaultNavigationOptions : {
-    headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-    },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
-}
+        initialRouteName: 'Categories',
+        defaultNavigationOptions : {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+        }
+    }
+);
+
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: MealsNavigator,
+    Favorites: FavoritesScreen
 });
 
-export default createAppContainer(MealsNavigator);
+
+export default createAppContainer(MealsFavTabNavigator);
